8e92432b4f49eff65ee1dafeabf6c0dce4c07f2f
NeoScraids <b.randon7127329@gmail.com>
2025-06-09 22:24:32 -0500

    Create prompts.py

---
+++ src/prompts.py
@@ -0,0 +1,8 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+def generate_completion(prompt, model_name="gpt2", max_length=50):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+    inputs = tokenizer(prompt, return_tensors="pt")
+    outputs = model.generate(**inputs, max_length=max_length)
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
